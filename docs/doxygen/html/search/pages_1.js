957a62d39a5478bfdf05d9bb32d10dab228cf1a7
kylon <3252255+kylon@users.noreply.github.com>
2026-02-25 22:31:02 +0100

    update doxygen site

--- docs/doxygen/html/search/pages_1.js
+++ docs/doxygen/html/search/pages_1.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['devices_0',['Supported devices',['../index.html#autotoc_md1',1,'']]],
-  ['docs_1',['Docs',['../index.html#autotoc_md4',1,'']]]
+  ['documentation_1',['Documentation',['../index.html#autotoc_md4',1,'']]]
 ];
