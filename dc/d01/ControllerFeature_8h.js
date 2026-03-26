5e182dc3de09f96478a1ffb8387ae5acf517c59b
kylon <3252255+kylon@users.noreply.github.com>
2026-03-26 15:08:36 +0100

    update docs

--- dc/d01/ControllerFeature_8h.js
+++ dc/d01/ControllerFeature_8h.js
@@ -4,6 +4,8 @@ var ControllerFeature_8h =
       [ "OWC::DeadZoneControlV1", "dc/d01/ControllerFeature_8h.html#aa586347c184be8c1bf6cd59e571fea4ea5597b0eba466cd779489842e70f05bad", null ],
       [ "OWC::ShoulderLedsV1", "dc/d01/ControllerFeature_8h.html#aa586347c184be8c1bf6cd59e571fea4eaa6725d31f5beba6844dd9e95a0de5ee4", null ],
       [ "OWC::RumbleV1", "dc/d01/ControllerFeature_8h.html#aa586347c184be8c1bf6cd59e571fea4ea1c25cad8213b6676878c0b797b5f82a4", null ],
-      [ "OWC::XinputMappingV1", "dc/d01/ControllerFeature_8h.html#aa586347c184be8c1bf6cd59e571fea4eafe1a24ef22361bd393487d6093217b71", null ]
+      [ "OWC::XinputMappingV1", "dc/d01/ControllerFeature_8h.html#aa586347c184be8c1bf6cd59e571fea4eafe1a24ef22361bd393487d6093217b71", null ],
+      [ "OWC::BackButton3", "dc/d01/ControllerFeature_8h.html#aa586347c184be8c1bf6cd59e571fea4ea7f30328bef015606d8d0bd2b47c70ecf", null ],
+      [ "OWC::BackButton4", "dc/d01/ControllerFeature_8h.html#aa586347c184be8c1bf6cd59e571fea4ea3e7f3175b643ceb3692b9b3a954f5475", null ]
     ] ]
 ];
--- search/all_0.js
+++ search/all_0.js
@@ -1,6 +1,8 @@
 var searchData=
 [
-  ['breathe_0',['Breathe',['../d8/d07/LedMode_8h.html#a72e72d4382a8481e05cf891d7186f404a0d9b0a3b3a93a8f759c9d8ac8eef97ca',1,'OWC']]],
-  ['button_1',['Button',['../d0/d01/Button_8h.html#a87f917092ee387b88f648ba3859c075e',1,'OWC']]],
-  ['button_2eh_2',['Button.h',['../d0/d01/Button_8h.html',1,'']]]
+  ['backbutton3_0',['BackButton3',['../dc/d01/ControllerFeature_8h.html#aa586347c184be8c1bf6cd59e571fea4ea7f30328bef015606d8d0bd2b47c70ecf',1,'OWC']]],
+  ['backbutton4_1',['BackButton4',['../dc/d01/ControllerFeature_8h.html#aa586347c184be8c1bf6cd59e571fea4ea3e7f3175b643ceb3692b9b3a954f5475',1,'OWC']]],
+  ['breathe_2',['Breathe',['../d8/d07/LedMode_8h.html#a72e72d4382a8481e05cf891d7186f404a0d9b0a3b3a93a8f759c9d8ac8eef97ca',1,'OWC']]],
+  ['button_3',['Button',['../d0/d01/Button_8h.html#a87f917092ee387b88f648ba3859c075e',1,'OWC']]],
+  ['button_2eh_4',['Button.h',['../d0/d01/Button_8h.html',1,'']]]
 ];
