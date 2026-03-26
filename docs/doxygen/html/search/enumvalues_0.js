4da38fe7eb7baa4583acab37357aa912c7734f24
kylon <3252255+kylon@users.noreply.github.com>
2026-03-26 15:08:36 +0100

    update docs

--- docs/doxygen/html/search/enumvalues_0.js
+++ docs/doxygen/html/search/enumvalues_0.js
@@ -1,4 +1,6 @@
 var searchData=
 [
-  ['breathe_0',['Breathe',['../d8/d07/LedMode_8h.html#a72e72d4382a8481e05cf891d7186f404a0d9b0a3b3a93a8f759c9d8ac8eef97ca',1,'OWC']]]
+  ['backbutton3_0',['BackButton3',['../dc/d01/ControllerFeature_8h.html#aa586347c184be8c1bf6cd59e571fea4ea7f30328bef015606d8d0bd2b47c70ecf',1,'OWC']]],
+  ['backbutton4_1',['BackButton4',['../dc/d01/ControllerFeature_8h.html#aa586347c184be8c1bf6cd59e571fea4ea3e7f3175b643ceb3692b9b3a954f5475',1,'OWC']]],
+  ['breathe_2',['Breathe',['../d8/d07/LedMode_8h.html#a72e72d4382a8481e05cf891d7186f404a0d9b0a3b3a93a8f759c9d8ac8eef97ca',1,'OWC']]]
 ];
