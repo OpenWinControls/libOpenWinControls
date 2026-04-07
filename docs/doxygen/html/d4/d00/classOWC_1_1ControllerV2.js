7655b14e5edf1213e536f08ee2d9ad533d4462b7
kylon <3252255+kylon@users.noreply.github.com>
2026-04-07 23:46:58 +0200

    update docs

--- docs/doxygen/html/d4/d00/classOWC_1_1ControllerV2.js
+++ docs/doxygen/html/d4/d00/classOWC_1_1ControllerV2.js
@@ -1,6 +1,6 @@
 var classOWC_1_1ControllerV2 =
 [
-    [ "ControllerV2", "d4/d00/classOWC_1_1ControllerV2.html#a53e4d302b637af50dd4c46e298caef56", null ],
+    [ "ControllerV2", "d4/d00/classOWC_1_1ControllerV2.html#ad26c62ce18431313aeea1b2d832c9e20", null ],
     [ "getAnalogBoundary", "d4/d00/classOWC_1_1ControllerV2.html#a85b2e2bef11fbf9cd21890a6b36be6f0", null ],
     [ "getAnalogCenter", "d4/d00/classOWC_1_1ControllerV2.html#a1fe2ac485f523f4579db58ab7053cf5f", null ],
     [ "getBackButton", "d4/d00/classOWC_1_1ControllerV2.html#a9f83dfd0c2f878dac4c78f735b364111", null ],
--- docs/doxygen/html/search/all_1.js
+++ docs/doxygen/html/search/all_1.js
@@ -3,7 +3,7 @@ var searchData=
   ['controller_0',['Controller',['../d3/d07/classOWC_1_1Controller.html',1,'OWC']]],
   ['controllerfeature_1',['ControllerFeature',['../dc/d01/ControllerFeature_8h.html#aa586347c184be8c1bf6cd59e571fea4e',1,'OWC']]],
   ['controllerfeature_2eh_2',['ControllerFeature.h',['../dc/d01/ControllerFeature_8h.html',1,'']]],
-  ['controllerv1_3',['ControllerV1',['../d7/d00/classOWC_1_1ControllerV1.html',1,'OWC::ControllerV1'],['../d7/d00/classOWC_1_1ControllerV1.html#adaba667a284dbd6040447c15f5a055af',1,'OWC::ControllerV1::ControllerV1()']]],
-  ['controllerv2_4',['ControllerV2',['../d4/d00/classOWC_1_1ControllerV2.html',1,'OWC::ControllerV2'],['../d4/d00/classOWC_1_1ControllerV2.html#a53e4d302b637af50dd4c46e298caef56',1,'OWC::ControllerV2::ControllerV2()']]],
+  ['controllerv1_3',['ControllerV1',['../d7/d00/classOWC_1_1ControllerV1.html',1,'OWC::ControllerV1'],['../d7/d00/classOWC_1_1ControllerV1.html#af2cdc16cd2659c07a584ef251e8c6636',1,'OWC::ControllerV1::ControllerV1()']]],
+  ['controllerv2_4',['ControllerV2',['../d4/d00/classOWC_1_1ControllerV2.html',1,'OWC::ControllerV2'],['../d4/d00/classOWC_1_1ControllerV2.html#ad26c62ce18431313aeea1b2d832c9e20',1,'OWC::ControllerV2::ControllerV2()']]],
   ['credits_5',['Credits',['../index.html#autotoc_md5',1,'']]]
 ];
