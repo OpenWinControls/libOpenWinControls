b3d5a7bee36155db6e3b92be695fea46eb3c6896
kylon <3252255+kylon@users.noreply.github.com>
2026-04-09 14:30:44 +0200

    remove old files

--- de/d02/Controller_8h.js
+++
@@ -1,4 +0,0 @@
-var Controller_8h =
-[
-    [ "OWC::Controller", "d3/d07/classOWC_1_1Controller.html", "d3/d07/classOWC_1_1Controller" ]
-];
--- search/files_7.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['xinputusageidmap_2eh_0',['XinputUsageIDMap.h',['../d6/d00/XinputUsageIDMap_8h.html',1,'']]]
-];
